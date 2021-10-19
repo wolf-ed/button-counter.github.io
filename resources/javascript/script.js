47820838f0a200b33ca6e6f8fe67e4459de94916
wolf-ed <wolf7ed@gmail.com>
2021-10-19 03:23:50 +0200

    repository created with one of my html-css-js templates

---
+++ resources/javascript/script.js
@@ -0,0 +1,19 @@
+let timesPressed = 0;
+
+//select button we will link
+let buttonSelector = document.querySelector('button');
+
+
+//function to print message with date and counter(of times pressed)
+buttonSelector.onclick = function(){
+    timesPressed++
+    const d = new Date();
+    const month = new Date()
+
+    //put together a message to print
+    let date = "<h2>Your pressed the button today, <br>day: " + d.getDate() + " of the month: "
+    + month.getMonth() + "<br><br><br><br> Number of times you pressed it: <br><br><br><span>" + timesPressed + "</span></h2>";
+    
+    //select where it will be printed
+    document.getElementById('placeToPrint').innerHTML = date;
+}
