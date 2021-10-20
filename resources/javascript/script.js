cfc3c63239a81d417601dbd70b3c8dcc737723c3
wolf-ed <wolf7ed@gmail.com>
2021-10-21 01:50:38 +0200

    html, script and css updated

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -3,16 +3,17 @@ let timesPressed = 0;
 //select button we will link
 let buttonSelector = document.querySelector('button');
 
-
 //function to print message with date and counter(of times pressed)
 buttonSelector.onclick = function(){
     timesPressed++
     const d = new Date();
-    const month = new Date()
-
+    let month = d.getMonth() + 1;
+    if(month < 10){
+        month = '0' + month;
+    }
     //put together a message to print
-    let date = "<h2>You pressed the button today: <br>Day: " + d.getDate() + ". Month: "
-    + month.getMonth() + "<br><br><br><br> Number of times you pressed it: <br><br><br><span>" + timesPressed + "</span></h2>";
+    let date = "<br><h2>You pressed the button today: <br>Day: " + d.getDate() + ". Month: "
+    + month + "<br> Number of times you pressed it: <br><span>" + timesPressed + "</span></h2>";
     
     //select where it will be printed
     document.getElementById('placeToPrint').innerHTML = date;
